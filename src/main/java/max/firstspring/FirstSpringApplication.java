a3979ac2e7580a305640d13437ab28b16764cb65
Maxwell Adomako <maxwell.adomako@amalitech.com>
2024-03-08 11:05:23 +0000

    first-spring project configs

---
+++ src/main/java/max/firstspring/FirstSpringApplication.java
@@ -0,0 +1,18 @@
+package max.firstspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// This is the spring app launcher
+// First Spring Demo Project
+@SpringBootApplication
+public class FirstSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FirstSpringApplication.class, args);
+		System.out.println("God is Great....");
+	}
+
+}
