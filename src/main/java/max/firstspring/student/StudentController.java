d8238dcb7976702da7b9051081f102c8279e528e
Maxwell Adomako <maxwell.adomako@amalitech.com>
2024-03-12 14:45:49 +0000

    added an end point

--- src/main/java/max/firstspring/student/StudentController.java
+++ src/main/java/max/firstspring/student/StudentController.java
@@ -1,6 +1,8 @@
 package max.firstspring.student;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/students")
 public class StudentController {
+
+   @GetMapping()
+    public List<String> findAllStudents(){
+        return List.of(
+          "Maxwell",
+          "Adomako"
+        );
+    }
 }
