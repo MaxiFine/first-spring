4e7ab8e0a9913977f7d098d71c5c5f5531f26463
Maxwell Adomako <maxwell.adomako@amalitech.com>
2024-03-12 14:35:22 +0000

    index page added, next is build

--- src/main/java/max/firstspring/FirstSpringApplication.java
+++ src/main/java/max/firstspring/FirstSpringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // This is the spring app launcher
 // First Spring Demo Project
 @SpringBootApplication
-public class FirstSpringApplication {
+public class  FirstSpringApplication {
 
 	public static void main(String[] args) {
 
---
+++ src/main/java/max/firstspring/student/StudentController.java
@@ -0,0 +1,12 @@
+package max.firstspring.student;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// annotating
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+}
